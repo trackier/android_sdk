5458dfa58b8f8ac313710ecc62da94a1fc6cfd45
Hemant Mann <hemant.mann121@gmail.com>
2021-01-15 19:03:40 +0530

    feat: add support for default events

--- TrackierSDK/sdk-core/src/main/java/com/cloudstuff/trackiersdk/TrackierEvent.kt
+++ TrackierSDK/sdk-core/src/main/java/com/cloudstuff/trackiersdk/TrackierEvent.kt
@@ -1,21 +1,37 @@
 package com.cloudstuff.trackiersdk
 
-import java.util.Date
-
 data class TrackierEvent(val name: String) {
-    var orderId: String? = null;
-    var currency: String? = null;
-    var param1: String? = null;
-    var param2: String? = null;
-    var param3: String? = null;
-    var param4: String? = null;
-    var param5: String? = null;
-    var param6: String? = null;
-    var param7: String? = null;
-    var param8: String? = null;
-    var param9: String? = null;
-    var param10: String? = null;
+    var orderId: String? = null
+    var currency: String? = null
+    var param1: String? = null
+    var param2: String? = null
+    var param3: String? = null
+    var param4: String? = null
+    var param5: String? = null
+    var param6: String? = null
+    var param7: String? = null
+    var param8: String? = null
+    var param9: String? = null
+    var param10: String? = null
 
-    var revenue: Double? = null;
-    val createdAt: String = Util.dateFormatter.format(Date())
+    var revenue: Double? = null
+    var eventValue = mutableMapOf<String, Any>()
+
+    companion object {
+        const val LEVEL_ACHIEVED = "xxx"
+        const val ADD_TO_CART = "xxx"
+        const val ADD_TO_WISHLIST = "xxx"
+        const val COMPLETE_REGISTRATION = "xxx"
+        const val TUTORIAL_COMPLETION = "xxx"
+        const val PURCHASE = "xxx"
+        const val SUBSCRIBE = "xxx"
+        const val START_TRIAL = "xxx"
+        const val ACHIEVEMENT_UNLOCKED = "xxx"
+        const val CONTENT_VIEW = "xxx"
+        const val TRAVEL_BOOKING = "xxx"
+        const val SHARE = "xxx"
+        const val INVITE = "xxx"
+        const val LOGIN = "xxx"
+        const val UPDATE = "xxx"
+    }
 }
