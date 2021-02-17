2f454afb41c5a51ff6f46bae3505638b81fe1f5e
prak24oct <prakharatdit@gmail.com>
2021-02-18 00:31:50 +0530

    sampla app in java and kotlin

---
+++ TrackierSDK/example-app-java/app/src/main/java/com/trackier/example_app_java/MainActivity.java
@@ -0,0 +1,43 @@
+package com.trackier.example_app_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.trackier.sdk.TrackierEvent;
+import com.trackier.sdk.TrackierSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button eventTrack, eventTrackWithCurrency;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eventTrack = findViewById(R.id.event_track);
+        eventTrackWithCurrency = findViewById(R.id.event_curr_track);
+        eventTrack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TrackierEvent event = new TrackierEvent(TrackierEvent.UPDATE);
+                event.param1 = "Param Name";
+                TrackierSDK.trackEvent(event);
+                Log.d("TAG", "onClick: event_track ");
+            }
+        });
+        eventTrackWithCurrency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TrackierEvent event = new TrackierEvent(TrackierEvent.UPDATE);
+                event.param1 = "Praram Name";
+                event.revenue = 0.5;
+                event.currency = "USD";
+                TrackierSDK.trackEvent(event);
+                Log.d("TAG", "onClick: eventTrackWithCurrency");
+            }
+        });
+    }
+}
---
+++ TrackierSDK/example-app-java/app/src/main/java/com/trackier/example_app_java/MainApplication.java
@@ -0,0 +1,32 @@
+package com.trackier.example_app_java;
+
+import android.app.Application;
+import android.content.Context;
+import com.trackier.sdk.TrackierSDK;
+import com.trackier.sdk.TrackierSDKConfig;
+
+public class MainApplication extends Application {
+
+    private static Context mContext;
+    private static MainApplication mInstance;
+    public static Context getmContext() {
+        return mContext;
+    }
+    public static void setmContext(Context mContext) {
+        MainApplication.mContext = mContext;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setmContext(getApplicationContext());
+        mInstance = this;
+
+        final String TR_DEV_KEY  = "c814db62-c196-4505-bc8b-46fa8e37f688";
+        TrackierSDKConfig sdkConfig = new   TrackierSDKConfig(this, TR_DEV_KEY,"test");
+        TrackierSDK.initialize(sdkConfig);
+    }
+    public static synchronized MainApplication getInstance() {
+        return mInstance;
+    }
+}
