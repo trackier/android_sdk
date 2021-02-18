b57327b9ab1874012cd5388e237ff2b668b5fb28
prak24oct <prakharatdit@gmail.com>
2021-02-18 12:34:24 +0530

    Sample App Updated in Kotlin and Java

---
+++ TrackierSDK/example-app-kotlin/app/src/main/java/com/trackier/example_app_kotlin/MainApplication.kt
@@ -0,0 +1,34 @@
+package com.trackier.example_app_kotlin
+
+import android.app.Application
+import android.content.Context
+import com.trackier.sdk.TrackierSDK
+import com.trackier.sdk.TrackierSDKConfig
+
+class MainApplication : Application() {
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MainApplication? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // initialize for any
+
+         val TR_DEV_KEY: String = "c814db62-c196-4505-bc8b-46fa8e37f688"
+
+        // Use ApplicationContext.
+        // example: SharedPreferences etc...
+        val context: Context = MainApplication.applicationContext()
+
+        val sdkConfig = TrackierSDKConfig(this, TR_DEV_KEY, "test")
+        TrackierSDK.initialize(sdkConfig)
+    }
+}
