bfcbc6d258d0fb01a13fc4c18e4efa06f7d5ef67
Hemant Mann <hemant.mann121@gmail.com>
2021-11-16 17:08:23 +0530

    update: sample java application

--- TrackierSDK/example-app-java/app/src/main/java/com/trackier/example_app_java/MainApplication.java
+++ TrackierSDK/example-app-java/app/src/main/java/com/trackier/example_app_java/MainApplication.java
@@ -3,6 +3,7 @@ package com.trackier.example_app_java;
 import android.app.Application;
 import android.content.Context;
 import com.trackier.sdk.TrackierSDK;
+import com.trackier.sdk.AttributionParams;
 import com.trackier.sdk.TrackierSDKConfig;
 
 public class MainApplication extends Application {
@@ -23,7 +24,11 @@ public class MainApplication extends Application {
         mInstance = this;
 
         final String TR_DEV_KEY  = "xxxx-xx-4505-bc8b-xx";
-        TrackierSDKConfig sdkConfig = new   TrackierSDKConfig(this, TR_DEV_KEY,"production");
+        TrackierSDKConfig sdkConfig = new TrackierSDKConfig(this, TR_DEV_KEY,"production");
+//        AttributionParams params = new AttributionParams();
+//        params.setParterId("ADD_PARTNER_ID_HERE");
+//        params.setChannel("ADD_CHANNEL_HERE"); // optional
+//        sdkConfig.setAttributionParams(params);
         TrackierSDK.initialize(sdkConfig);
     }
     public static synchronized MainApplication getInstance() {
