162826cd89abd50a8959d7fd6d40004136c27338
Hemant Mann <hemant.mann121@gmail.com>
2023-07-19 16:57:40 +0530

    Update TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Util.kt

--- TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Util.kt
+++ TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Util.kt
@@ -327,7 +327,6 @@ object Util {
         val activationTimestamp = buildDate!!.toLong() * 1000L
         val activationDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
             .format(Date(activationTimestamp))
-        logger.info("Activation Date = $activationDate")
         return activationDate
     }
     
