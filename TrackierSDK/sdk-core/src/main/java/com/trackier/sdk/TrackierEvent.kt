6ecb3a911dd020d9f22ee23a91c24884e64770e2
Sanu gupta <sanugupta266@gmail.com>
2024-06-03 11:48:40 +0530

    feat-add-productid

--- TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/TrackierEvent.kt
+++ TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/TrackierEvent.kt
@@ -8,6 +8,8 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class TrackierEvent(val id: String) {
     @JvmField var orderId: String? = null
+    @Json(name = "product_id")
+    @JvmField var productId: String?= null
     @JvmField var currency: String? = null
     @JvmField var param1: String? = null
     @JvmField var param2: String? = null
