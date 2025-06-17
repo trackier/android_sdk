c3c07b74a3930bbedd704a086a1b8b53aeff9cf8
Sanu gupta <sanugupta266@gmail.com>
2025-06-17 11:21:02 +0530

    update: APIRepository.kt

--- TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Constants.kt
+++ TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
     const val USER_AGENT = "com.cloudstuff.trackiersdk:trackier-android:" + SDK_VERSION
     const val API_VERSION = "v1"
     const val BASE_URL = "events.trackier.io/" + API_VERSION + "/"
-    const val BASE_URL_DL = "https://sdkr.apptracking.io/dl/" //"https://eo6vxppz8n3ly04.m.pipedream.net/"
-    const val BASE_URL_DYNAMIC_LINK = "https://sdkr.apptracking.io/api/v4/ug/dlg/"
+    const val BASE_URL_DL = "sdkr.apptracking.io/dl/" //"https://eo6vxppz8n3ly04.m.pipedream.net/"
+    const val BASE_URL_DYNAMIC_LINK = "sdkr.apptracking.io/api/v4/ug/dlg/"
     const val LOG_TAG = "trackiersdk"
     const val LOG_WORK_TAG = "trackiersdk:work"
     const val LOG_WORK_INPUT_KEY = "trackiersdk:work_request"
--- TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Factory.kt
+++ TrackierSDK/sdk-core/src/main/java/com/trackier/sdk/Factory.kt
@@ -4,6 +4,7 @@ import java.util.logging.Logger
 import java.util.logging.Level
 
 object Factory {
+    @Volatile
     private var config: TrackierSDKConfig? = null
     val logger: Logger = Logger.getLogger(Constants.LOG_TAG)
     init {
